f4ba07a7d7635c3f53a3f59008cd86a7158d4b80
Robert <67601105+michalskirobert@users.noreply.github.com>
2020-10-23 20:21:03 +0200

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,77 +1,45 @@
+ window.addEventListener("DOMContentLoaded", function() {
 
-function sendMail() {
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
 
-  const successMsg = "Your e-mail has been sent succesfully. <br /> Thank you!";
-  const failMsg =
-    "I'm sorry, your message couldn't be sent. <br /> Please try one more time";
-
-  const emailValid = document.querySelector(".emailValid");
-  const nameValid = document.querySelector(".nameValid");
-  const msgValid = document.querySelector(".messageValid");
-
-  const email = document.querySelector("#email").value;
-  const name = document.querySelector("#name").value;
-  const msg = document.querySelector("#msg").value;
-
-  if (name === "") {
-    nameValid.innerHTML = "Name is required ✘";
-    nameValid.style.color = "red";
-    document.querySelector("#name").style.borderBottom = "2px solid red";
-    document.querySelector("#name").focus();
-    return false;
-
-  } else {
-    nameValid.style.color = "#52E5BA";
-    nameValid.innerHTML = "Name ✓";
-    document.querySelector("#name").style.border = "1px solid #52E5BA";
-  }
-
-  if (email === "") {
-    emailValid.innerHTML = "E-mail is required ✘";
-    emailValid.style.color = "red";
-    document.querySelector("#email").style.borderBottom = "2px solid red";
-    document.querySelector("#email").focus();
-    return false;
-  } else {
-    emailValid.style.color = "#52E5BA";
-    emailValid.innerHTML = "E-mail ✓";
-    document.querySelector("#email").style.border = "1px solid #52E5BA";
-  }
-
-  if (msg === "") {
-    msgValid.innerHTML = "Message is required ✘";
-    msgValid.style.color = "red";
-    document.querySelector("#msg").style.borderBottom = "2px solid red";
-    document.querySelector("#msg").focus();
-    return false;
-  } else {
-    msgValid.style.color = "#52E5BA";
-    msgValid.innerHTML = "Message ✓";
-    document.querySelector("#msg").style.border = "1px solid #52E5BA";
-  }
-
-
-  document.querySelector("#msg").style.border = "1px solid #52E5BA";
-
-  var formdata = new FormData();
-  formdata.append("email", email);
-  formdata.append("name", name);
-  formdata.append("message", msg);
-
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("sent").style.display = "block";
-      document.getElementById("error").style.display = "none";
-      document.getElementById("sent").innerHTML = successMsg;
-    } else if (this.readyState == 4 && this.status != 200) {
-      document.getElementById("error").style.display = "block";
-      document.getElementById("sent").style.display = "none";
-      document.getElementById("error").innerHTML = failMsg;
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks!";
     }
-  };
 
-  xhttp.open("POST", "./send.php", true);
-  xhttp.send(formdata);
-}
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
