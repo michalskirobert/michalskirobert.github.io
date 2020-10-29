a24da8aa913180da50210eadf89f7acce2a59cd5
michalskirobert <majointa@gmail.com>
2020-10-29 23:06:12 +0100

    update portfolio

--- portfolio/website/01/js/index.js
+++ portfolio/website/01/js/index.js
@@ -1,9 +1,20 @@
+window.onscroll= () => {
+  const header = document.querySelector("header");
+
+  if (document.documentElement.scrollTop > 80) {
+    header.classList.add("active");
+  }else {
+    header.classList.remove("active");
+  }
+}
+
 function sendButton() {
   let button = document.getElementById("button");
   let comm = document.getElementById("hidden__communicate");
   if ((comm.style.display = "none")) {
     comm.style.display = "block";
     comm.innerHTML = "Your request has been sent";
+    comm.style.marginBottom ="25px";
     button.style.color = "white";
     button.style.background = "green";
   } else {
