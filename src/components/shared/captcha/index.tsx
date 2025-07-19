3d54723775d7b6a6045f9a8984918ee8fe2db628
Robert <majointa@gmail.com>
2025-07-19 19:50:38 +0200

    fix/loading-form

--- src/components/shared/captcha/index.tsx
+++ src/components/shared/captcha/index.tsx
@@ -71,37 +71,39 @@ export default function TextCaptcha({
     getCaptcha();
   }, []);
 
-  if (!captcha) return <CaptchaSkeleton />;
-
   return (
     <LoadingBlocker isLoading={isVeryfing || !!isLoading}>
       {children}
-      <div>
-        <CustomImage
-          src={captcha.image}
-          alt="captcha"
-          className="mb-2"
-          title="captcha"
-          placeholder="empty"
-        />
-        <div className="flex gap-1">
-          <input
-            type="text"
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-            disabled={isVeryfing}
-            className={`${invalidClassName} mt-0.5 w-full p-2 rounded  shadow-sm sm:text-sm focus-visible:outline-[var(--clr-accent)] transition-colors duration-300`}
-            placeholder="Enter the text shown"
+      {!captcha ? (
+        <CaptchaSkeleton />
+      ) : (
+        <div>
+          <CustomImage
+            src={captcha.image}
+            alt="captcha"
+            className="mb-2"
+            title="captcha"
+            placeholder="empty"
           />
-          <CustomButton
-            {...restButtonParams}
-            isLoading={isLoading || isVeryfing}
-            content={content || "Verify"}
-            onClick={submit}
-          />
+          <div className="flex gap-1">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              disabled={isVeryfing}
+              className={`${invalidClassName} mt-0.5 w-full p-2 rounded  shadow-sm sm:text-sm focus-visible:outline-[var(--clr-accent)] transition-colors duration-300`}
+              placeholder="Enter the text shown"
+            />
+            <CustomButton
+              {...restButtonParams}
+              isLoading={isLoading || isVeryfing}
+              content={content || "Verify"}
+              onClick={submit}
+            />
+          </div>
+          {alert && <span className="text-xs text-red-400">{alert}</span>}
         </div>
-        {alert && <span className="text-xs text-red-400">{alert}</span>}
-      </div>
+      )}
     </LoadingBlocker>
   );
 }
