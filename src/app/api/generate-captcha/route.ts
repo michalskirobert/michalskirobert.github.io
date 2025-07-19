5ff92e357980fffa95bfeeecb591d85ec18902cb
Robert <majointa@gmail.com>
2025-07-19 19:59:07 +0200

    fix captcha

--- src/app/api/generate-captcha/route.ts
+++ src/app/api/generate-captcha/route.ts
@@ -1,13 +1,33 @@
 import { NextResponse } from "next/server";
 import { CaptchaGenerator } from "captcha-canvas";
+import { registerFont } from "canvas";
 import jwt from "jsonwebtoken";
+import path from "path";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
+registerFont(
+  path.join(
+    process.cwd(),
+    "src",
+    "app",
+    "lib",
+    "fonts",
+    "OpenSans-Regular.ttf"
+  ),
+  {
+    family: "OpenSans",
+  }
+);
+
 export async function GET() {
   const captcha = new CaptchaGenerator({ width: 150, height: 50 });
-  const buffer = await captcha.generate();
 
+  captcha.setCaptcha({
+    font: "35px OpenSans",
+  });
+
+  const buffer = await captcha.generate();
   const base64 = buffer.toString("base64");
   const text = captcha.text;
 
